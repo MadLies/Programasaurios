3bc7de43d699c04d25f8f2fd123a0c96fd6f69be
VenusBaquero <vbaquero@unal.edu.co>
2022-11-25 22:39:20 -0500

    roar in forget dino

--- Divicuenta/src/components/Forget.js
+++ Divicuenta/src/components/Forget.js
@@ -1,28 +1,27 @@
 import React from 'react'
-import { View, Text, Modal, ScrollView, Pressable , StyleSheet , TextInput, Image, Alert} from 'react-native'
+import { View, Text, Modal, ScrollView, Pressable, StyleSheet, TextInput, Image, Alert } from 'react-native'
 import { forgetStyle as styles } from '../styles/ForgetStyle';
+import playRoar from '../functions/PlayRoar';
 
 const Forget = ({
-modalForget,
-setModalForget,
+    modalForget,
+    setModalForget,
 
-})=>
-
-{  
+}) => {
     const [email, setEmail] = React.useState('');
 
     const handleEmail = () => {
-        if (email==''){
-           Alert.alert(
-            'Error',
-            'Por favor ingresa un correo valido',
-            [{Text: 'OK'}]
+        if (email == '') {
+            Alert.alert(
+                'Error',
+                'Por favor ingresa un correo valido',
+                [{ Text: 'OK' }]
             )
 
         }
     }
 
-    return(
+    return (
         <View style={styles.container}>
             <Image
                 style={styles.imageBack}
@@ -30,19 +29,19 @@ setModalForget,
             />
             <ScrollView>
                 <View style={styles.header}>
-                <Text style={styles.title}>Olvide mi contraseña    </Text>
-                    
-                        <Pressable
-                        style={styles.close} 
+                    <Text style={styles.title}>Olvide mi contraseña    </Text>
+
+                    <Pressable
+                        style={styles.close}
                         onPress={() => setModalForget(false)}
-                        >
-                            <Image  
+                    >
+                        <Image
                             style={styles.close}
                             source={require('../img/exit.png')}
-                            >
-                            </Image>
-                        </Pressable>
-                    
+                        >
+                        </Image>
+                    </Pressable>
+
                 </View>
 
                 <View style={styles.container2}>
@@ -52,34 +51,40 @@ setModalForget,
                 <View style={styles.form}>
                     <Text style={styles.label}>Correo electrónico</Text>
                     <TextInput
-                    style={styles.input}
-                    keyboardType='email-address'
-                    placeholder='Correo electrónico'
-                    placeholderTextColor={'#666'}
-                    onChangeText={(text) => setEmail(text)}
-                    value={email}
+                        style={styles.input}
+                        keyboardType='email-address'
+                        placeholder='Correo electrónico'
+                        placeholderTextColor={'#666'}
+                        onChangeText={(text) => setEmail(text)}
+                        value={email}
                     />
                 </View>
 
                 <View style={styles.container3}>
                     <Pressable
-                    style={styles.button}
-                    onPress={() => handleEmail()}
+                        style={styles.button}
+                        onPress={() => handleEmail()}
                     >
                         <Text style={styles.textButton}>Enviar</Text>
                     </Pressable>
                 </View>
-           
+
                 <View style={styles.container4}>
-                    <Image
-                        style={styles.image}
-                        source={require('../img/Dino.png')}
-                    />
+                    <Pressable     
+                        onPress={()=> playRoar()}
+                    >
+                        <Image
+                            style={styles.image}
+                            source={require('../img/Dino.png')}
+                        />
+
+                    </Pressable>
+
                 </View>
             </ScrollView>
         </View>
     )
-    
+
 }
 
 export default Forget
